f61ba5861536dbc4359ef0fe89a5c92e5b0ebe91
Anhelina Rabtsevych <anhelina@ondaytrip.com>
2024-01-06 00:01:28 +0100

    components added

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './Button.module.css';
+
+const Button = ({ onLoadMore }) => {
+  return (
+      <button
+        type="button"
+        className={css.Button}
+        onClick={() => {
+          onLoadMore();
+        }}
+      >
+        Load more
+      </button>
+  );
+};
+
+Button.propTypes = {
+  onLoadMore: PropTypes.func.isRequired,
+};
+
+export default Button;
